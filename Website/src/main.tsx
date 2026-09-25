7f7a0c4135064f5dac9591bcab79819a352b6510
Arpan Adhikari <adhikariarpan2063@gmail.com>
2026-09-25 18:24:03 +0545

    Integrate Vercel Speed Insights

--- Website/src/main.tsx
+++ Website/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import App from "./App.tsx";
 import "./index.css";
 import "./firebase";
@@ -14,5 +15,6 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
     <Analytics />
+    <SpeedInsights />
   </StrictMode>,
 );
